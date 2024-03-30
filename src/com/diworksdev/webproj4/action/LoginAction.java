ae123a804ca465717ee9f6857f20ee025afff4d6
Your Name <you@example.com>
2024-03-30 20:21:27 +0900

    second

---
+++ src/com/diworksdev/webproj4/action/LoginAction.java
@@ -0,0 +1,54 @@
+package com.diworksdev.webproj4.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.diworksdev.webproj4.dao.LoginDAO;
+import com.diworksdev.webproj4.dto.LoginDTO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+    private String username;
+    private String password;
+    private List<LoginDTO> loginDTOList = new ArrayList<>();
+
+    public String execute() {
+        String ret = ERROR;
+        System.out.println(username);
+        System.out.println(password);
+        LoginDAO dao = new LoginDAO();
+        loginDTOList = dao.select(username, password);
+        if (!loginDTOList.isEmpty() && this.username.equals(loginDTOList.get(0).getUsername())
+                && this.password.equals(loginDTOList.get(0).getPassword())) {
+            ret = SUCCESS;
+        } else {
+            ret = ERROR;
+        }
+        return ret;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<LoginDTO> getLoginDTOList() {
+        return loginDTOList;
+    }
+
+    public void setLoginDTOList(List<LoginDTO> loginDTOList) {
+        this.loginDTOList = loginDTOList;
+    }
+}
+
